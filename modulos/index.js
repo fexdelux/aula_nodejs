705cfb7c0bbeaebd4f6b83d259feb8c2387ea192
Fernando Henrique Souza <fernando.souza@urcx.com.br>
2023-02-04 20:58:32 -0300

    refatoração

--- modulos/index.js
+++ modulos/index.js
@@ -1,30 +1,14 @@
 const readline = require('readline-sync');
 
-const CalculoMaster = require('./modulos/calculo-master');
 const Interpretador = require('./modulos/interpretador');
 
 
-let expressao = readline.question('Qual a conta quer que eu resolva?');
 let resultado = 'Expressão inválida!';
-let calc = new CalculoMaster();
+let expressao = readline.question('Qual a conta quer que eu resolva?');
 let interpretador = new Interpretador(expressao);
 
+resultado = interpretador.resultado();
 
 
-if(interpretador.validarCalculo('+')) {
-    resultado = calc.soma(interpretador.valor1,interpretador.valor2);
-}
-
-if(interpretador.validarCalculo('x')) {
-    resultado = calc.multiplicacao(interpretador.valor1,interpretador.valor2);
-}
-
-if(interpretador.validarCalculo('-')) {
-    resultado = calc.subtracao(interpretador.valor1,interpretador.valor2);
-}
-
-if(interpretador.validarCalculo('/')) {
-    resultado = calc.divisao(interpretador.valor1,interpretador.valor2);
-}
 
 console.log('resultado é:',resultado);
--- modulos/modulos/interpretador.js
+++ modulos/modulos/interpretador.js
@@ -1,18 +1,37 @@
-
+const CalculoMaster = require('./calculo-master');
 class Interpretador {
 
     constructor(expressao) {
         this.expressao = expressao;
         this.valor1 = 0;
         this.valor2 = 0;
+        this.identificador = [
+            {sinal: '+', metodo: 'soma'},
+            {sinal: '-', metodo: 'subtracao'},
+            {sinal: 'x', metodo: 'multiplicacao'},
+            {sinal: '/', metodo: 'divisao'}
+        ];
+        this.metodo = null;
+        this.identificarModulo();
     }
 
-    validarCalculo(identificador) {
-        let resultado = this.expressao.indexOf(identificador) > -1;
+    resultado(){
+        let calc = new CalculoMaster();
+        return  calc[this.metodo](this.valor1, this.valor2);
+    }
+    preencherValores(identificador) {
         let valores = this.expressao.split(identificador);
         this.valor1 = parseInt(valores[0]);
         this.valor2 = parseInt(valores[1]);
-        return resultado;
+    }
+
+    identificarModulo() {
+        this.identificador.forEach( (value) => {
+            if(this.expressao.indexOf(value.sinal) > -1) {
+                this.metodo = value.metodo;
+                this.preencherValores(value.sinal);
+            }
+        })
     }
 }
 
